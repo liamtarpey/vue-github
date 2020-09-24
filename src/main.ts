1d093751c108deb80b2446579735626919686598
Liam Tarpey <liam.m.tarpey@gmail.com>
2020-09-24 16:29:25 +0100

    Configure some base styles

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
+import App from '@/App.vue';
+import router from '@/routes/router';
+import '@/assets/styles/main.scss';
 
+/** TODO: What is this? */
 Vue.config.productionTip = false;
 
+/** Init app */
 new Vue({
   router,
   render: (h) => h(App),
